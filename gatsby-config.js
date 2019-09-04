6e8611540b41b308699d6e4dd50f958e9c18af60
D-Nice <d-nice2@protonmail.com>
2019-09-04 15:45:06 -0400

    feat: set sharp plugin to use mozjpeg
    may take longer but should provide better compression on images used by the site

--- gatsby-config.js
+++ gatsby-config.js
@@ -35,6 +35,13 @@ module.exports = {
       }
     },
     'gatsby-transformer-sharp',
-    'gatsby-plugin-sharp'
+    {
+      resolve: 'gatsby-plugin-sharp',
+      options: {
+        useMozJpeg: true,
+        stripMetadata: true,
+        defaultQuality: 64
+      }
+    }
   ]
 }
